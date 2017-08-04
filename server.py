2b70a186d8baee6afad70bc24696a5785c0e9d17
grantwbrooks <grantwbrooks@gmail.com>
2017-08-03 21:33:46 -0400

    landing page done

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template('ninjas.html')
+
+@app.route('/dojos')
+def dojos():
+    return render_template('dojos.html')
+
+@app.route('/dojoform', methods=['POST'])
+def create_user():
+   print "Got Post Info"
+   name = request.form['name']
+   email = request.form['email']
+   print name
+   return redirect('/dojos')
+
+
+
+
+
+app.run(debug=True)
